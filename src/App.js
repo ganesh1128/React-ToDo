77969724e2787e9fe14eb2160d7c4e0aa076f08f
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-08-07 17:07:23 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [list,setList] = useState([
+    {
+      id: 1,
+      taskName: "Go to Office",
+      isDone: false
+    },
+    {
+      id: 2,
+      taskName: "Have Lunch",
+      isDone: false
+    },
+    {
+      id: 3,
+      taskName: "Completed Work",
+      isDone: false
+    },
+    {
+      id: 4,
+      taskName: "Came Home",
+      isDone: false
+    },
+    {
+      id: 5,
+      taskName: "Have Dinner",
+      isDone: false
+    }      
+  ])
+
+
+  let changeStatus = (id,checkStatus) => {
+    let taskIndex = list.findIndex((obj) => obj.id === id);
+   checkStatus ?  list[taskIndex].isDone = true : list[taskIndex].isDone = false;
+    setList([...list])
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <ul>
+      {
+        list.map((ele) => {
+          return <li type="none" style={{textDecoration: ele.isDone ? "line-through" : " "}}><input type="checkbox"
+          onChange={(e) => {changeStatus(ele.id, e.target.checked)}} /> {ele.taskName} </li>
+          
+        })
+      }
+    </ul>
+
     </div>
   );
+ 
+  
 }
 
 export default App;
